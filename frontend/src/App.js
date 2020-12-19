e055549fc6f3a9159522272bea63e6976ab00f58
MinSoo Lee <minsoo.lee@8byte.co.kr>
2020-12-19 22:11:25 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [lists, setLists] = useState([]);
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    // 여기서 데이터베이스에 있는 값을 가져온다.
+    axios.get(`/api/values`).then((response) => {
+      console.log('response', response);
+      setLists(response.data);
+    });
+  }, []);
+
+  const changeHandler = (event) => {
+    setValue(event.currentTarget.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    axios.post(`/api/value`, { value: value }).then((response) => {
+      if (response.data.success) {
+        console.log('response', response);
+        setLists([...lists, response]);
+        setValue('');
+      } else {
+        alert('값을 DB에 넣는데 실패했습니다.');
+      }
+    });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {lists && lists.map((list, index) => <li key={index}>{list.value}</li>)}
+
+        <form className="container" onSubmit={submitHandler}>
+          <input onChange={changeHandler} value={value} type="text" placeholder="입력해주세요..." />
+          <button type="submit">확인</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
